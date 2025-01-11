68a3aabb653f0f62e06a4e609a6eccd884871243
Filip Řeháček <filiprehacek4@gmail.com>
2025-01-11 13:12:08 +0100

    hope it works

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "bg1": "#0B022D",
+        "bg2": "#200094",
+        "purp": "#9f62f0"
+      },
+      fontFamily: {
+        kode: ['Kode Mono', 'monospace'],
+      }
+    },
+    screens: {
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    }
+  },
+  plugins: [],
+}
+
